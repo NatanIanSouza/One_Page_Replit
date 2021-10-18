4511bd1a1f1c19ceb7b3a4e91f68561b90f9a356
NatanIanSouza <85906728+NatanIanSouza@users.noreply.github.com>
2021-10-18 14:25:35 +0000

    Initial commit

---
+++ js/menu.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $('#btn-menu').click(function(e){
+        e.preventDefault()
+        $('#menu').toggle('slow')
+    })
+
+    //Criar uma função para monitorar os links
+    $('a').click(function(e){
+    e.preventDefault()
+    let url = $(this).attr('href');
+
+    if(url == 'contact'){
+        $('#'+url).show();
+        $('#home').hide();
+        $('#action').hide();
+        $('#about').hide();
+    }
+
+    else if (url == 'home'){
+        $('#'+url).show();
+        $('#contact').hide();
+        $('#action').hide();
+        $('#about').hide();
+    }
+    
+    else if (url == 'action'){
+        $('#'+url).show();
+        $('#contact').hide();
+        $('#home').hide();
+        $('#about').hide();
+    }
+
+    else if (url == 'about'){
+        $('#'+url).show();
+        $('#contact').hide();
+        $('#action').hide();
+        $('#home').hide();
+    }
+    })
+})
